e2e68a00335ec6241478c8ae33eff6d9e94f3d7c
ayushdotdev <ayush438500@gmail.com>
2026-01-11 19:57:07 +0530

    telescope setup

---
+++ after/plugins/telescope.lua
@@ -0,0 +1,4 @@
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = 'Telescope live grep' })
