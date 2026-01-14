af88b9e8769614bfabbcef5323c2977690795009
ayushdotdev <ayush438500@gmail.com>
2026-01-14 22:47:40 +0530

    Need to setup my lsp again

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,15 +1,16 @@
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
-vim.opt.wrap = true
-vim.opt.breakindent = true
+vim.opt.wrap = false
+-- vim.opt.breakindent = true
 
 vim.opt.cursorline = true
 vim.opt.fillchars = { eob = ' '}
 
 vim.opt.clipboard = "unnamedplus"
+vim.opt.swapfile =  false
